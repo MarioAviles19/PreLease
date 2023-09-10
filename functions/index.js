99c0217add4dcede14589462c32ffc68435161a6
Mario Aviles <mjaviles360@gmail.com>
2023-09-10 14:17:36 -0500

    Updated functions to include a firestore trigger that updates the Info document in the RentalHealthCheck collection on document written

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require("firebase-functions");
+const { onDocumentCreated, onDocumentWritten } = require("firebase-functions/v2/firestore");
+
+const {initializeApp} = require("firebase-admin/app")
+const {getFirestore,FieldValue} = require("firebase-admin/firestore")
+
+const app = initializeApp();
+const db = getFirestore()
+  
+let ssrServerServer;
+exports.ssrServer = functions.region("us-central1").https.onRequest(async (request, response) => {      
+  if (!ssrServerServer) {
+    functions.logger.info("Initialising SvelteKit SSR entry");
+    ssrServerServer = require("./ssrServer/index").default;
+    functions.logger.info("SvelteKit SSR entry initialised!");
+  }
+  functions.logger.info("Requested resource: " + request.originalUrl);
+  return ssrServerServer(request, response);
+});
+
+exports.updateRentalHealthCheckInfo = onDocumentWritten("RentalHealthChecks/{docId}", async (event)=>{
+
+  const document = event.data.after;
+  const data = document.data();
+
+  const gender = data.gender;
+
+  if( document.id == "Info"){
+    return;
+  }
+
+  if(!document.exists){
+    db.doc("RenatlHealthChecks/Info").set({count: FieldValue.increment(-1)},{merge:true})
+
+    return;
+  }
+ const infoSnapshot = await db.doc('RentalHealthChecks/Info').get();
+
+
+  await db.doc("RentalHealthChecks/Info").set({count: FieldValue.increment(1), emergent: data.emergency == "Yes"? FieldValue.increment(1): FieldValue.increment(0), gender: {...infoSnapshot.data().gender, [gender] : FieldValue.increment(1)}}, {merge:true})
+  await db.doc("Tests/HealtCheck").set({count : FieldValue.increment(1)}, {merge:true})
+  return;
+
+})
